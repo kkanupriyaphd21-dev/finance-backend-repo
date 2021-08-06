cac568cbfbfddffcf5446f89a96743ad930cabdf
DEV_C <DEV_C@example.com>
2021-08-06 15:14:58 +0530

    wire the initial project files

--- core/mixins.py
+++ core/mixins.py
@@ -6,3 +6,5 @@ class LoginRequiredMixin(object):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+# c095 2021-08-06T15:14:58 wire the initial project files
