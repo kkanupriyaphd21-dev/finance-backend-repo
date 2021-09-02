cf7b46c049141d557992e87024c737ad33420ab3
DEV_C <DEV_C@example.com>
2021-09-02 15:26:22 +0530

    wire the initial project files

--- micro_admin/views.py
+++ micro_admin/views.py
@@ -1117,3 +1117,5 @@ def user_change_password(request):
 # c093 2021-08-01T12:52:44 tighten bootstrap config
 
 # c100 2021-08-17T13:09:33 verify: deployment entrypoint
+
+# c107 2021-09-02T15:26:22 wire the initial project files
