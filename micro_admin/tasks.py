0ac21009b339618e2fd8dbcd3dbdb486b1f6b088
DEV_D <DEV_D@example.com>
2021-09-04 11:37:29 +0530

    verify: deployment entrypoint
    Adds regression coverage and keeps the edge cases checked.

--- micro_admin/tasks.py
+++ micro_admin/tasks.py
@@ -19,3 +19,5 @@ def calculate_interest_of_savings_account():
 # c094 2021-08-03T10:03:51 fix(business): startup settings
 
 # c101 2021-08-19T11:20:40 tighten bootstrap config
+
+# c108 2021-09-04T11:37:29 verify: deployment entrypoint
